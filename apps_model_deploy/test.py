1370f484ca64fbba04e37342fc7ed1c88bde4dcc
skdarkey <skdarkey@gmail.com>
2024-07-01 02:05:05 +0200

    updatind test.py for aws endpoint

--- apps_model_deploy/test.py
+++ apps_model_deploy/test.py
@@ -1,13 +1,12 @@
 import predict
 import requests
 
-# testing before webservice
 app_data = {
-    "Category" : 'GAME',  #Eg: FINANCE, FAMILY, PARENTING, MEDICAL, GAME, SOCIAL, BUSINESS, PERSONALIZATION
+    "Category" : 'MEDICAL',  #Eg: FINANCE, FAMILY, PARENTING, MEDICAL, GAME, SOCIAL, BUSINESS, PERSONALIZATION
     "Type" : 'Paid',
     "Content_Rating" : 'Teen', # Everyone, Teen, Everyone 10+, 
-    "Size_MBs" : '15.0' ,  
-    "Price" : '5' ,
+    "Size_MBs" : '50.0' ,  
+    "Price" : '4.99' ,
 }
 
 # testing locally
@@ -15,7 +14,14 @@ app_data = {
 # install = predict.predict(features)
 
 # testing on web-service
-url = 'http://localhost:9696/predict'    # specify the endpoiint "predict" which is used in the flask app
+# url = 'http://localhost:9696/predict'       # local endpoint
+url = 'http://13.60.29.10:9696/predict'       # AWS endpoint
 response = requests.post(url, json=app_data)
-print(response.json())
+predicted_installs = response.json()
 
+print(f"if the App category is {app_data['Category']} " \
+      f"and it is a {app_data['Type']} " \
+      f"app and its Content_Rating is {app_data['Content_Rating']} " \
+      f"with a Size of about {app_data['Size_MBs']} " \
+      f"and Price of {app_data['Price']} " \
+      f"then predicted potential number of downloads is {predicted_installs['number-of-installs']}")
